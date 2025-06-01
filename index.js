6f3b2c11185f32ec671acfc4f6a1d0b5fca43c69
maryamshehzadi768@gmail.com <maryamshehzadi768@gmail.com>
2025-06-01 13:27:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
